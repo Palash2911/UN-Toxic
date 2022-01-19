8cef7781f82c8b4fbdfff2e55f2878fc098059ab
Palash Rambhia <palashrambhia2@gmail.com>
2022-01-19 14:34:05 +0530

    Added Authentication

--- app/src/main/java/com/godspeed/un_toxic/MainActivity.kt
+++ app/src/main/java/com/godspeed/un_toxic/MainActivity.kt
@@ -1,11 +1,148 @@
 package com.godspeed.un_toxic
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.godspeed.un_toxic.databinding.ActivityMainBinding
+import com.google.firebase.FirebaseException
+import com.google.firebase.FirebaseTooManyRequestsException
+import com.google.firebase.auth.*
+import com.google.firebase.auth.R
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "LoginActivity"
+    private val auth = FirebaseAuth.getInstance()
+    private var storedVerificationId: String = ""
+    private lateinit var binding: ActivityMainBinding
+    private val db = Firebase.firestore
+
+    override fun onStart() {
+        super.onStart()
+        binding.layoutLoadingProfile.visibility = View.VISIBLE
+        binding.authCardView.visibility = View.GONE
+//        if(auth.currentUser != null){
+//            db.collection("Profiles").document(auth.currentUser!!.uid).get()
+//                .addOnCompleteListener{task->
+//                    if(task.result?.exists() == true){
+//                        val intent = Intent(this, Homepage::class.java)
+//                        startActivity(intent)
+//                        finish()
+//                    } else {
+//                        val intent = Intent(this, UserProfile::class.java)
+//                        startActivity(intent)
+//                        finish()
+//                    }
+//
+//                }
+//        } else {
+            binding.layoutLoadingProfile.visibility = View.VISIBLE
+            binding.authCardView.visibility = View.GONE
+//        }
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(com.godspeed.un_toxic.R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.get.setOnClickListener{sendOtp()}
+        binding.verify.setOnClickListener{verifyOtp()}
+    }
+
+    private fun sendOtp(){
+        if (binding.phone.text.isEmpty() || binding.phone.text.length < 10){
+            Toast.makeText(this, "Please enter valid phone number!", Toast.LENGTH_SHORT).show()
+            return
+        }
+        binding.authProgress.visibility = View.VISIBLE
+        val phoneNumber = "+91 " + binding.phone.text.toString()
+
+        val options = PhoneAuthOptions.newBuilder(auth)
+            .setPhoneNumber(phoneNumber)
+            .setTimeout(60, TimeUnit.SECONDS)
+            .setActivity(this)
+            .setCallbacks(callbacks)
+            .build()
+        PhoneAuthProvider.verifyPhoneNumber(options)
+    }
+    private fun verifyOtp(){
+        if (binding.otp.text.isEmpty() || binding.otp.text.length < 6){
+            Toast.makeText(this, "Please enter valid 6 digit OTP!", Toast.LENGTH_SHORT).show()
+            return
+        }
+        val otpNumber = binding.otp.text.toString()
+        val credential = PhoneAuthProvider.getCredential(storedVerificationId, otpNumber)
+        signInWithPhoneAuthCredential(credential)
+    }
+
+    private val callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+            Log.d(TAG, "onVerificationCompleted: $credential")
+            binding.authProgress.visibility = View.GONE
+            binding.layoutPhone.visibility = View.GONE
+            signInWithPhoneAuthCredential(credential)
+        }
+
+        override fun onVerificationFailed(e: FirebaseException) {
+            Log.w(TAG, "onVerificationFailed", e)
+
+            if (e is FirebaseAuthInvalidCredentialsException) {
+                Toast.makeText(applicationContext, "Invalid Request! Contact Developer!", Toast.LENGTH_SHORT).show()
+            } else if (e is FirebaseTooManyRequestsException) {
+                // The SMS quota for the project has been exceeded
+                Toast.makeText(applicationContext, "SMS Quota Reached! Contact Developer!", Toast.LENGTH_SHORT).show()
+            }
+            binding.authProgress.visibility = View.GONE
+        }
+
+        override fun onCodeSent(
+            verificationId: String,
+            token: PhoneAuthProvider.ForceResendingToken
+        ) {
+            // The SMS verification code has been sent to the provided phone number, we
+            // now need to ask the user to enter the code and then construct a credential
+            // by combining the code with a verification ID.
+            Log.d(TAG, "onCodeSent:$verificationId")
+            Toast.makeText(this@MainActivity, "OTP Sent Successfully!", Toast.LENGTH_SHORT).show()
+            // Save verification ID and resending token so we can use them later
+            storedVerificationId = verificationId
+//            resendToken = token
+            binding.authProgress.visibility = View.GONE
+            binding.layoutPhone.visibility = View.GONE
+            binding.layoutOtp.visibility = View.VISIBLE
+        }
+    }
+
+    private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+                    val intent = Intent(this, ContactsContract.Profile::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    // Sign in failed, display a message and update the UI
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    if (task.exception is FirebaseAuthInvalidCredentialsException) {
+                        // The verification code entered was invalid
+                        Toast.makeText(this, "Invalid code, Try Again!", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this, "Something went wrong!", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                binding.authProgress.visibility = View.GONE
+            }
     }
 }
