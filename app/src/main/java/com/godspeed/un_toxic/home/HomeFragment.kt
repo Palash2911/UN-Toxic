48c47de4c51936e9f20bc9b093073968d6492754
Palash Rambhia <palashrambhia2@gmail.com>
2022-01-21 21:47:04 +0530

    Update

--- app/src/main/java/com/godspeed/un_toxic/home/HomeFragment.kt
+++ app/src/main/java/com/godspeed/un_toxic/home/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.godspeed.un_toxic.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,12 +9,17 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.RecyclerView
 import com.godspeed.un_toxic.databinding.FragmentProgressBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
     private var _binding: FragmentProgressBinding? = null
+    val db = Firebase.firestore
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -30,15 +36,17 @@ class HomeFragment : Fragment() {
         _binding = FragmentProgressBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.tv15
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
         return root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        db.collection("Profiles").get().addOnSuccessListener {
+            binding.nameuser.text = "Name" + ""
+        }
+            .addOnFailureListener { exception ->
+                Log.d("TAG", "Error getting documents: ", exception)
+//                Toast.makeText(this,"Error getting documents: ", Toast.LENGTH_SHORT).show()
+            }
     }
 }
