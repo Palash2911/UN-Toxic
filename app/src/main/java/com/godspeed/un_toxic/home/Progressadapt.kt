bb8fdf17b07c989596a8bfbc621f3c21e0d92153
Palash Rambhia <palashrambhia2@gmail.com>
2022-01-25 13:55:56 +0530

    Update

--- app/src/main/java/com/godspeed/un_toxic/home/Progressadapt.kt
+++ app/src/main/java/com/godspeed/un_toxic/home/Progressadapt.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.godspeed.un_toxic.R
