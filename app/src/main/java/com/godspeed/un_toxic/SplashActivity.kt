526f8806c7d641466c2a2b4db9bc4706910db994
Aditya Bonde <aditya.bonde1152@gmail.com>
2022-01-26 22:19:18 +0530

    UI Changes done

---
+++ app/src/main/java/com/godspeed/un_toxic/SplashActivity.kt
@@ -0,0 +1,29 @@
+package com.godspeed.un_toxic
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import android.widget.TextView
+import androidx.appcompat.app.ActionBar
+
+class SplashActivity : AppCompatActivity() {
+    lateinit var label:TextView ;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.requestFeature(Window.FEATURE_ACTION_BAR)
+        supportActionBar?.hide();
+
+        setContentView(R.layout.activity_splash)
+        label = findViewById(R.id.label)
+
+
+    Handler().postDelayed({
+        val intent = Intent(this,MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    },3000)
+
+    }
+}
