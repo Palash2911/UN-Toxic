bdd95585d6aa7ab63b6e1024e2312fdd52345f12
Aditya Bonde <aditya.bonde1152@gmail.com>
2022-01-31 12:19:26 +0530

    Add funds fixed

--- app/src/main/java/com/godspeed/un_toxic/updateprofile.kt
+++ app/src/main/java/com/godspeed/un_toxic/updateprofile.kt
@@ -7,18 +7,19 @@ import android.util.Log
 import android.widget.Toast
 import com.godspeed.un_toxic.databinding.ActivityProfileBinding
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 class updateprofile : AppCompatActivity() {
     private lateinit var binding: ActivityProfileBinding
     private val TAG = "Profiles"
-    private val db = Firebase.firestore
+    private lateinit var db:FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
-
+        db = FirebaseFirestore.getInstance();
         binding = ActivityProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
